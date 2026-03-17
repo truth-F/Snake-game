c324fd140dd24e050fc12889a950f7fa2765df67
truth-F <truthfranco501@gmail.com>
2026-03-17 13:08:28 -0700

    Add files via upload

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include  <conio.h>
-#include <window>
 #include <vector>
+using namespace std;
+
+enum Direction { UP, DOWN, LEFT, RIGHT };
+struct Position {
+    int x;
+    int y;
+};
+Position move(Position current, Direction d) {
+    Position newPos = current;
+
+    switch (d) {
+        case UP:
+            newPos.y +=1;
+            break;
+        case DOWN:
+            newPos.y -=1;
+            break;
+        case RIGHT:
+            newPos.x +=1;
+            break;
+        case LEFT:
+            newPos.x -=1;
+            break;
+    }
+    return newPos;
+}
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Position head = {5,5};
+    cout << head.x << " " << head.y << endl;
+    head = move(head, DOWN);
+    cout << head.x << " " << head.y << endl;
+    head = move(head, RIGHT);
+    cout << head.x << " " << head.y << endl;
+
     return 0;
 }
