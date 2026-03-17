5c38506d5885925e61a6584f5217210537dfa413
truth-F <truthfranco501@gmail.com>
2026-03-17 16:22:55 -0700

    Add files via upload

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 #include  <conio.h>
 #include <vector>
-using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
 
 enum Direction { UP, DOWN, LEFT, RIGHT };
 struct Position {
     int x;
     int y;
 };
+
+bool isOpposite(Direction a, Direction b) {
+    return (a == UP && b == DOWN)||
+           (a == DOWN && b == UP)||
+           (a == RIGHT && b == LEFT)||
+           (a == LEFT && b == RIGHT);
+}
+
 Position move(Position current, Direction d) {
     Position newPos = current;
-
     switch (d) {
-        case UP:
-            newPos.y +=1;
-            break;
-        case DOWN:
-            newPos.y -=1;
-            break;
-        case RIGHT:
-            newPos.x +=1;
-            break;
-        case LEFT:
-            newPos.x -=1;
-            break;
+        case RIGHT: newPos.x +=1; break;
+        case LEFT:  newPos.x -=1; break;
+        case UP:    newPos.y +=1; break;
+        case DOWN:  newPos.y -=1; break;
     }
     return newPos;
 }
 
+int main(){
+    vector<Position> snake = {
+        {5,5},
+        {4,5},
+        {3,5}
+    };
+    Position head = snake[0];
+    Position prev = snake[1];
+    Direction currentDirection = RIGHT;
+    Direction nextDirection = UP;
+    cout << head.x << " " << head.y << endl;
 
+    if (!isOpposite(currentDirection, nextDirection)) {
+        currentDirection = nextDirection;
+    }
+    head = move(head, currentDirection);
+    cout << head.x << " " << head.y << endl;
 
-int main() {
-   
 
     return 0;
+
 }
